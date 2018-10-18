be2814f836f84736a228d01c57112c8aede701ed
Paul Breugnot <paul.breugnot@etu.emse.fr>
2018-10-18 18:16:44 +0200

    Debbug and refactor prints

--- firmware/flash_manager.cpp
+++ firmware/flash_manager.cpp
@@ -1,7 +1,7 @@
 #include "flash_manager.h"
 #include "nvs_flash.h"
 #include "nvs.h"
-#include "errors.h"
+#include "setup_uart.h"
 
 nvs_handle my_handle;
 
@@ -48,7 +48,7 @@ err_t write_tag(const char *rfid_tag, const char *value) {
   }
 }
 
-check_tag_result check_tag(const char *rfid_tag, const char *output_value) {
+check_tag_result check_tag(const char *rfid_tag, char **output_value) {
   /*
    * Check if rfid_tag is a valid tag.
    * If its a valid tag, result is stored in output_value.
@@ -56,23 +56,47 @@ check_tag_result check_tag(const char *rfid_tag, const char *output_value) {
    * with output_value set NULL.
    */
   size_t value_length;
-
   // Check if the tag is valid (a.k.a is a valid nvs key) and eventually 
   // store the length of the associated value in value_length for later.
   esp_err_t err = nvs_get_str(my_handle, rfid_tag, NULL, &value_length);
+  char* read_value = (char *) malloc(value_length);
   if (err == ESP_ERR_NVS_NOT_FOUND) {
     return INVALID_TAG;
   }
   else if (err == OK) {
     // The tag is valid, and the associated value is stored in output_value
-    if (output_value != NULL) {
-      nvs_get_str(my_handle, rfid_tag, output_value, &value_length);
+    if (read_value != NULL) {
+      nvs_get_str(my_handle, rfid_tag, read_value, &value_length);
+      free(*output_value);
+      *output_value = read_value;
     }
     return VALID_TAG;
   }
   else {
     return READ_ERROR;
   }
+}
+
+esp_err_t delete_tag(const char *rfid_tag) {
+  /*
+   * Remove a tag from the nvs, so the tag become invalid.
+   */
+  esp_err_t err = nvs_erase_key(my_handle, rfid_tag);
+  if (err != ESP_OK) {
+    return err;
+  }
+  return nvs_commit(my_handle);
+}
+
+esp_err_t delete_all_tags() {
+  /*
+   * Remove all tags.
+   */
+  esp_err_t err = nvs_erase_all(my_handle);
+  if (err != ESP_OK) {
+    return err;
+  }
+  return nvs_commit(my_handle);
 }
 
 
--- firmware/flash_manager.h
+++ firmware/flash_manager.h
@@ -1,5 +1,6 @@
 // Functions to manage eeprom (e.g. to save/modify/delete RFID tags)
 #include "errors.h"
+#include "esp_err.h"
 
 #define INVALID_TAG 0
 #define VALID_TAG 1
@@ -9,4 +10,6 @@ typedef int check_tag_result;
 
 err_t init_nvs();
 err_t write_tag(const char *rfid_tag, const char *value);
-check_tag_result check_tag(const char *rfid_tag, const char *output_value);
+check_tag_result check_tag(const char *rfid_tag, char **output_value);
+esp_err_t delete_tag(const char *rfid_tag);
+esp_err_t delete_all_tags();
