b4be96c12630bcb3dc68a306a0d0ce30f4db65c9
NicolasLagaillardie <lagaillardie.nicolas@live.fr>
2018-11-07 00:18:03 +0100

    add test API

---
+++ web_server/Chapitre_10/js/article.js
@@ -0,0 +1,13 @@
+
+var form = document.querySelector("form");
+form.addEventListener("submit", function(e) {
+  e.preventDefault();
+  var article = new FormData(form);
+  ajaxPost("https://oc-jswebsrv.herokuapp.com/article", article, function(
+    reponse
+  ) {
+    var messageElt = document.createElement("p");
+    messageElt.textContent = "L'article a bien été ajouté.";
+    document.getElementById("resultat").appendChild(messageElt);
+  });
+});
