354e545df72e8afa1821a1eb589d929b35fa2292
NicolasLagaillardie <lagaillardie.nicolas@live.fr>
2018-11-11 23:32:30 +0100

    added ajax call and modify button

--- web_server/html/script.js
+++ web_server/html/script.js
@@ -1,4 +1,45 @@
 // ----------------------------------------------------------------------------
+// Ajax calls
+// ----------------------------------------------------------------------------
+
+function ajaxGet(url, callback) {
+    var req = new XMLHttpRequest();
+    req.open("GET", url);
+    req.addEventListener("load", function () {
+        if (req.status >= 200 && req.status < 400) {
+            // Appelle la fonction callback en lui passant la réponse de la requête
+            callback(req.responseText);
+        } else {
+            console.error(req.status + " " + req.statusText + " " + url);
+        }
+    });
+    req.addEventListener("error", function () {
+        console.error("Erreur réseau avec l'URL " + url);
+    });
+    req.send(null);
+}
+
+function ajaxPost(url, data, callback, isJson) {
+    var req = new XMLHttpRequest();
+    req.open("POST", url);
+    req.addEventListener("load", function () {
+        if (req.status >= 200 && req.status < 400) {
+            callback(req.responseText);
+        } else {
+            console.error(req.status + " " + req.statusText + " " + url);
+        }
+    });
+    req.addEventListener("error", function () {
+        console.error("Erreur réseau avec l'URL " + url);
+    });
+    if (isJson) {
+        req.setRequestHeader("Content-Type", "application/json");
+        data = JSON.stringify(data);
+    }
+    req.send(data);
+}
+
+// ----------------------------------------------------------------------------
 // Include HTML files
 // ----------------------------------------------------------------------------
 
@@ -34,21 +75,39 @@ const divAddUserForm = document.getElementById("user_buttons");
 
 const add_button = document.getElementById("add_button");
 
-add_button.onclick = function () {
+var indexLine = 0;
+
+function addHTMLForm(index = -1) {
     const divForm = document.createElement('div');
-    divForm.setAttribute('w3-include-html', 'form.html');
+    divForm.setAttribute('w3-include-html', 'addForm.html');
     divAddUserForm.appendChild(divForm);
     myHTMLInclude();
-    formResponse();
+    formResponse(index);
+}
+
+add_button.onclick = function () {
+    addHTMLForm();
 };
 
-function formResponse() {
+function formResponse(index) {
     const addUserForm = document.getElementById("add_user_form");
     if (addUserForm) {
-        
+
         const formScanRFID = document.getElementById("rfid_tag_form");
-        
-        formScanRFID.style.display = 'flex';
+
+        const rfidID = document.getElementById("rfidID");
+
+        formScanRFID.addEventListener("submit", function (e) {
+            e.preventDefault();
+
+            ajaxPost("https://oc-jswebsrv.herokuapp.com/api/lien", "Request ID", function () {
+                ajaxGet("https://oc-jswebsrv.herokuapp.com/api/liens", function (reponse) {
+                    var jsonData = JSON.parse(reponse);
+                    rfidID.value = jsonData[0].rfid;
+                });
+            }, true);
+
+        });
 
         addUserForm.addEventListener("submit", function (e) {
             e.preventDefault();
@@ -62,6 +121,52 @@ function formResponse() {
             console.log(infosForm);
 
             addUserForm.remove();
+
+            ajaxPost("https://oc-jswebsrv.herokuapp.com/api/lien", infosForm, function () {
+                ajaxGet("https://oc-jswebsrv.herokuapp.com/api/liens", function (reponse) {
+                    var jsonData = JSON.parse(reponse);
+
+                    if (index == -1) {
+
+                        const line = document.createElement('tr');
+
+                        const name = document.createElement('td');
+                        name.id = "name_" + indexLine;
+                        name.innerHTML = jsonData[0].name;
+
+                        const rfid = document.createElement('td');
+                        rfid.id = "rfid_" + indexLine;
+                        rfid.innerHTML = jsonData[0].rfid;
+
+                        const description = document.createElement('td');
+                        description.id = "description_" + indexLine;
+                        description.innerHTML = jsonData[0].description;
+
+                        const modify = document.createElement('td');
+                        const modifyButton = document.createElement('button');
+
+                        modifyButton.onclick = function () {
+                            if (addUserForm) {
+                                addUserForm.remove();
+                            }
+                            addHTMLForm(indexLine);
+                        };
+                        
+                        indexLine += 1;
+                        
+                    } else {
+                        const name = document.getElementById("name_" + index);
+                        name.innerHTML = jsonData[0].name;
+
+                        const rfid = document.getElementById("rfid_" + index);
+                        rfid.innerHTML = jsonData[0].rfid;
+
+                        const description = document.getElementById("description_" + index);
+                        description.innerHTML = jsonData[0].description;                        
+                    }
+                });
+            }, true);
+
         });
 
     } else {
