9fb7093e5247e181379caf7235f77f704e7467fd
NicolasLagaillardie <lagaillardie.nicolas@live.fr>
2018-11-12 14:29:19 +0100

    keep on continue security

--- web_server/html/index.js
+++ web_server/html/index.js
@@ -1,4 +1,29 @@
-/ ----------------------------------------------------------------------------
+const cookieParser = require('cookie-parser')
+const csrf = require('csurf')
+const bodyParser = require('body-parser')
+const express = require('express')
+ 
+// setup route middlewares 
+const csrfProtection = csrf({ cookie: true })
+const parseForm = bodyParser.urlencoded({ extended: false })
+ 
+// create express app 
+const app = express()
+ 
+// parse cookies 
+// we need this because "cookie" is true in csrfProtection 
+app.use(cookieParser())
+ 
+app.get('/form', csrfProtection, function(req, res) {
+  // pass the csrfToken to the view 
+  res.render('send', { csrfToken: req.csrfToken() })
+})
+ 
+app.post('/process', parseForm, csrfProtection, function(req, res) {
+  res.send('data is being processed')
+})
+
+// ----------------------------------------------------------------------------
 // Ajax calls
 // ----------------------------------------------------------------------------
 
