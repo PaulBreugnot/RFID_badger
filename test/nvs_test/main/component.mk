5db8f787e5d883a24a810cbb949877394c2cbfbf
Paul Breugnot <paul.breugnot@etu.emse.fr>
2018-11-12 19:43:06 +0100

    Move components

--- test/nvs_test/main/component.mk
+++ test/nvs_test/main/component.mk
@@ -2,5 +2,5 @@
 # "main" pseudo-component makefile.
 #
 # (Uses default behaviour of compiling all source files in directory, adding 'include' to include path.)
-COMPONENT_EXTRA_INCLUDES := $(PROJECT_PATH)/../../firmware_esp/components/nvs/include
-COMPONENT_SRCDIRS := . ../../../firmware_esp/components/nvs/
+COMPONENT_EXTRA_INCLUDES := $(PROJECT_PATH)/../../firmware/components/nvs/include
+COMPONENT_SRCDIRS := . ../../../firmware/components/nvs/
