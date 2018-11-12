f301b37bcae0006f48ad053c5f760409fad0432d
Paul Breugnot <32193009+PaulBreugnot@users.noreply.github.com>
2018-11-12 18:59:23 +0100

    Delete install_lib.sh

--- test/install_lib.sh
+++
@@ -1,8 +0,0 @@
-# This shell script allows you to hard link firmware files in your Arduino library repository in order to use them in test sketchs.
-# Hard link usage allow you to keep the "library" synchronized with the actual code files in firmware/ if you modify them.
-# Your Arduino Sketchbook repository must be specified in input.
-# You can find its path in the Arduino IDE, under file -> preferences -> Sketchbook location.
-
-arduino_libraries_path=$1/libraries
-mkdir $arduino_libraries_path/RFID_badger
-ln -f ../firmware/* $arduino_libraries_path/RFID_badger
