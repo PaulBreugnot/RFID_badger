b7f21cb53c7861050367aee0653912e5b2082138
Paul Breugnot <paul.breugnot@etu.emse.fr>
2018-11-12 19:40:14 +0100

    Move components

--- test/rfid_test/main/component.mk
+++ test/rfid_test/main/component.mk
@@ -2,4 +2,5 @@
 # "main" pseudo-component makefile.
 #
 # (Uses default behaviour of compiling all source files in directory, adding 'include' to include path.)
-
+COMPONENT_EXTRA_INCLUDES := $(PROJECT_PATH)/../../firmware/components/cpp_utils/
+COMPONENT_SRCDIRS := . ../../../firmware/components/cpp_utils/
