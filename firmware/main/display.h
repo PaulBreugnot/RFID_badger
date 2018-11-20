de36636ed60b4f56579ed562aca6c761cf582f9f
Paul Breugnot <paul.breugnot@etu.emse.fr>
2018-11-20 10:05:41 +0100

    Add display to firmware

---
+++ firmware/main/display.h
@@ -0,0 +1,4 @@
+#include "u8g2.h"
+#include "u8g2_esp32_hal.h"
+
+void init_display();
--- firmware/main/main.cpp
+++ firmware/main/main.cpp
@@ -2,12 +2,14 @@
 */
 
 #include "nvs_manager.h"
+// #include "display.h"
 
 extern "C" {
    void app_main();
+   #include "display.h"
 }
 
 void app_main()
 {
-
+	init_display();
 }
